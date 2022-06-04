637eb24c723717a06db7769ef0df47af6959f898
pooriavakili <pooriavakili09@gmail.com>
2022-06-04 20:35:28 +0430

    firstcommit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "../index.css"
-createApp(App).mount('#app')
+import store from './store'
+import router from "./router/router"
+createApp(App).use(router).use(store).mount('#app')
